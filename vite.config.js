ab67b2ac24be11af391b42c97669fe7bd05eb0c2
YUSUF KARABEY <yusuf_karabey@outlook.com>
2026-01-28 15:17:45 +0300

    Github Pages deploy edildi

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/RadioTedu-Classic/', //pages için
+  base: '/RadioTedu-Stream/',
 })
